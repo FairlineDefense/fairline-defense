2b1ce54f02c15cdcf7a77d595d4544f86aee7186
Jeffrey Wood <jwfirenze@gmail.com>
2022-10-07 20:31:32 -0700

    use functional components

--- client/components/Register/Signup.js
+++ client/components/Register/Signup.js
@@ -1,11 +1,8 @@
 import React from 'react'
-import {connect} from 'react-redux'
-import PropTypes from 'prop-types'
 import {signup} from '../../store'
 import {useDispatch} from 'react-redux'
 
-const Signup = props => {
-  const {name, displayName, error} = props
+const Signup = () => {
   const dispatch = useDispatch()
 
   const handleSubmit = evt => {
@@ -48,38 +45,3 @@ const Signup = props => {
   )
 }
 export default Signup
-/**
- * CONTAINER
- *   Note that we have two different sets of 'mapStateToProps' functions -
- *   one for Login, and one for Signup. However, they share the same 'mapDispatchToProps'
- *   function, and share the same Component. This is a good example of how we
- *   can stay DRY with interfaces that are very similar to each other!
- */
-// const mapLogin = state => {
-//   return {
-//     name: 'login',
-//     displayName: 'Login',
-//     error: state.user.error
-//   }
-// }
-
-// const mapSignup = state => {
-//   return {
-//     name: 'signup',
-//     displayName: 'Sign Up',
-//     error: state.user.error
-//   }
-// }
-
-// export const Login = connect(mapLogin, mapDispatch)(AuthForm)
-// export const Signup = connect(mapSignup, mapDispatch)(AuthForm)
-
-/**
- * PROP TYPES
- */
-// AuthForm.propTypes = {
-//   name: PropTypes.string.isRequired,
-//   displayName: PropTypes.string.isRequired,
-//   handleSubmit: PropTypes.func.isRequired,
-//   error: PropTypes.object
-// }
