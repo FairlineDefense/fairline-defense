2c0d596e3d814aeff43b6af6b8e1650d62b5693f
Jeffrey Wood <jwfirenze@gmail.com>
2022-10-18 16:43:01 -0400

    labels on personal info form

--- client/components/Portal/MembershipSubPortal/EditPersonalInformation.js
+++ client/components/Portal/MembershipSubPortal/EditPersonalInformation.js
@@ -28,13 +28,24 @@ flex-direction: row;
 margin-bottom: 2rem;
 justify-content: flex-start;
 `
+const Label = styled.label`
+display: block;
+font-weight: 300;
+font-size: 14px;
+font-weight: 400;
+margin-bottom: .5rem;
+`
 const Input = styled.input`
 padding: 1rem;
 border: 1px solid #AAB1B9;
 border-radius: 4px;
 outline: none;
-width: 220px;
+width: 260px;
 margin-right: 2rem;
+font-size: 16px;
+font-weight: 200;
+display: block;
+color: var(--primary);
 `
 const Button = styled.button`
 color: var(--blueblack);
@@ -45,14 +56,14 @@ border-radius: 5px;
 padding: .75rem;
 background: transparent;
 outline: none;
-margin: .5rem 0rem .5rem 0rem;
+margin: .5rem 1rem .5rem 0rem;
 font-size: 18px;
 font-weight: 200;
 cursor: pointer;
 `
 const CyanButton = styled.button`
 color: #fff;
-width: 220px;
+width: 200px;
 border: none;
 border-radius: 5px;
 padding: .75rem;
@@ -67,25 +78,50 @@ cursor: pointer;
         <Wrapper>
             <form>
                 <InputGroup>
-                <Input placeholder={'First Name'} name='firstName' value={form.firstName}></Input>
-                <Input placeholder={'Last Name'} name='lastName' value={form.lastName}></Input>
+                <span>
+                    <Label htmlFor={'firstName'}>First Name</Label>
+                        <Input placeholder={'First Name'} name='firstName' value={form.firstName}></Input>
+                    </span>
+                    <span>
+                    <Label htmlFor={'lastName'}>Last Name</Label>
+                        <Input placeholder={'Last Name'} name='lastName' value={form.lastName}></Input>
+                </span>
                 </InputGroup>
                 <InputGroup>
-                <Input placeholder={'Email Address'} name='email' value={form.email}></Input>
-                <Input placeholder={'Phone Number'} name='phone' value={form.phone}></Input>
+                <span>
+                    <Label htmlFor={'email'}>Email</Label>
+                        <Input placeholder={'Email Address'} name='email' value={form.email}></Input>
+                    </span>
+                    <span>
+                    <Label htmlFor={'phone'}>Phone</Label>
+                        <Input placeholder={'Phone Number'} name='phone' value={form.phone}></Input>
+                </span>
                 </InputGroup>
                 <InputGroup>
-                <Input placeholder={'Street Address'} name='streetAddress' value={form.streetAddress}></Input>
-                <Input placeholder={'City, Zip Code'} name='zipCode' value={form.city + form.zipCode}></Input>
+                <span>
+                    <Label htmlFor={'streetAddress'}>Street Address</Label>
+                        <Input placeholder={'Street Address'} name='streetAddress' value={form.streetAddress}></Input>
+                    </span>
+                    <span>
+                    <Label htmlFor={'city'}>City, Zip Code</Label>
+                        <Input placeholder={'City, Zip Code'} name='city' value={form.city + form.zipCode}></Input>
+                </span>
                 </InputGroup>
                 <InputGroup>
-                <Input placeholder={'Password'} name='password' value={form.password}></Input>
-                <Input placeholder={'Repeat Password'} name='repeatPassword' value={form.repeatPassword}></Input>
+                <span>
+                    <Label htmlFor={'password'}>Password</Label>
+                        <Input placeholder={'Password'} name='password' value={form.password}></Input>
+                    </span>
+                    <span>
+                    <Label htmlFor={'repeatPassword'}>Repeat New Password</Label>
+                        <Input placeholder={'Repeat Password'} name='repeatPassword' value={form.repeatPassword}></Input>
+                </span>
                 </InputGroup>
                 <InputGroup>
                 <Button>Cancel</Button><CyanButton>Save Edits</CyanButton>
                 </InputGroup>
-            </form>
+           </form>
         </Wrapper>
+
     )
 }
