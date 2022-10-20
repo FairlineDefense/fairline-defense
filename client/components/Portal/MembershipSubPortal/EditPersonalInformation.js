47a92d46b7e8543306ed0949388ee5ae073e4e94
Jeffrey Wood <jwfirenze@gmail.com>
2022-10-20 11:59:06 -0400

    state drop down on personal info

--- client/components/Portal/MembershipSubPortal/EditPersonalInformation.js
+++ client/components/Portal/MembershipSubPortal/EditPersonalInformation.js
@@ -47,7 +47,7 @@ font-size: 16px;
 font-weight: 200;
 color: var(--primary);
 `
-const ZipCode = styled.input`
+const State = styled.select`
 padding: 1rem;
 border: 1px solid #AAB1B9;
 border-radius: 4px;
@@ -123,18 +123,19 @@ const submitHandler = (e) => {
             return setErrorText('Invalid zip code.')
         }
         //Not operational:
-        if (password !== confirmPassword) {
-          return setErrorText('Passwords do not match.')
-        }
-        if (password.length < 8) {
-          return setErrorText('Your password must be at least 8 characters')
-        }
-        if (password.search(/[a-z]/i) < 0) {
-          return setErrorText('Your password must contain at least one letter.')
-        }
-        if (password.search(/[0-9]/) < 0) {
-          return setErrorText('Your password must contain at least one digit.')
-        } else {
+        // if (password !== confirmPassword) {
+        //   return setErrorText('Passwords do not match.')
+        // }
+        // if (password.length < 8) {
+        //   return setErrorText('Your password must be at least 8 characters')
+        // }
+        // if (password.search(/[a-z]/i) < 0) {
+        //   return setErrorText('Your password must contain at least one letter.')
+        // }
+        // if (password.search(/[0-9]/) < 0) {
+        //   return setErrorText('Your password must contain at least one digit.')
+        // } 
+        else {
             dispatch(update(form))
         }
       }
@@ -148,52 +149,105 @@ const submitHandler = (e) => {
                 <InputGroup>
                 <span>
                     <Label htmlFor="firstName">First Name</Label>
-                        <Input placeholder="First Name" name='firstName' value={form.firstName} onChange={(e)=>changeHandler(e)} required></Input>
+                        <Input type="text" placeholder="First Name" name='firstName' value={form.firstName} onChange={(e)=>changeHandler(e)} required></Input>
                     </span>
                     <span>
                     <Label htmlFor="lastName">Last Name</Label>
-                        <Input placeholder="Last Name" name='lastName' value={form.lastName} onChange={(e)=>changeHandler(e)} required></Input>
+                        <Input type="text" placeholder="Last Name" name='lastName' value={form.lastName} onChange={(e)=>changeHandler(e)} required></Input>
                 </span>
                 </InputGroup>
                 <InputGroup>
                 <span>
                     <Label htmlFor="email">Email</Label>
-                        <Input placeholder="Email Address" name='email' value={form.email} onChange={(e)=>changeHandler(e)} required></Input>
+                        <Input type="text" placeholder="Email Address" name='email' value={form.email} onChange={(e)=>changeHandler(e)} required></Input>
                     </span>
                     <span>
                     <Label htmlFor="phone">Phone</Label>
-                        <Input placeholder="Phone Number" name='phone' value={form.phone} onChange={(e)=>changeHandler(e)} required></Input>
+                        <Input type="text" placeholder="Phone Number" name='phone' value={form.phone} onChange={(e)=>changeHandler(e)} required></Input>
                 </span>
                 </InputGroup>
                 <InputGroup>
                 <span>
                     <Label htmlFor="streetAddress">Street Address</Label>
-                        <Input placeholder="Street Address" name='streetAddress' value={form.streetAddress} onChange={(e)=>changeHandler(e)} required></Input>
+                        <Input type="text" placeholder="Street Address" name='streetAddress' value={form.streetAddress} onChange={(e)=>changeHandler(e)} required></Input>
                     </span>
                     <span>
-                    <Label htmlFor="city">City, Zipcode</Label>
-                        <City placeholder="City" name='city' value={form.city} onChange={(e)=>changeHandler(e)} required></City>
-                        <ZipCode placeholder="Zip." name='zipCode' value={form.zipCode} onChange={(e)=>changeHandler(e)} required></ZipCode>
+                    <Label htmlFor="city">City, State</Label>
+                        <City type="text" placeholder="City" name='city' value={form.city} onChange={(e)=>changeHandler(e)} required></City>
+                        <State placeholder="State" name='state' value={form.state} onChange={(e)=>changeHandler(e)} required>
+                        <option value="AL">Alabama</option>
+                        <option value="AK">AK</option>
+  <option value="AL">AL</option>
+  <option value="AR">AR</option>
+  <option value="AZ">AZ</option>
+  <option value="CA">CA</option>
+  <option value="CO">CO</option>
+  <option value="CT">CT</option>
+  <option value="DC">DC</option>
+  <option value="DE">DE</option>
+  <option value="FL">FL</option>
+  <option value="GA">GA</option>
+  <option value="HI">HI</option>
+  <option value="IA">IA</option>
+  <option value="ID">ID</option>
+  <option value="IL">IL</option>
+  <option value="IN">IN</option>
+  <option value="KS">KS</option>
+  <option value="KY">KY</option>
+  <option value="LA">LA</option>
+  <option value="MA">MA</option>
+  <option value="MD">MD</option>
+  <option value="ME">ME</option>
+  <option value="MI">MI</option>
+  <option value="MN">MN</option>
+  <option value="MO">MO</option>
+  <option value="MS">MS</option>
+  <option value="MT">MT</option>
+  <option value="NC">NC</option>
+  <option value="ND">ND</option>
+  <option value="NE">NE</option>
+  <option value="NH">NH</option>
+  <option value="NJ">NJ</option>
+  <option value="NM">NM</option>
+  <option value="NV">NV</option>
+  <option value="NY">NY</option>
+  <option value="OH">OH</option>
+  <option value="OK">OK</option>
+  <option value="OR">OR</option>
+  <option value="PA">PA</option>
+  <option value="RI">RI</option>
+  <option value="SC">SC</option>
+  <option value="SD">SD</option>
+  <option value="TN">TN</option>
+  <option value="TX">TX</option>
+  <option value="UT">UT</option>
+  <option value="VA">VA</option>
+  <option value="VT">VT</option>
+  <option value="WA">WA</option>
+  <option value="WI">WI</option>
+  <option value="WV">WV</option>
+  <option value="WY">WY</option>
+                        </State>
                 </span>
                 </InputGroup>
                 <InputGroup>
                 <span>
                     <Label htmlFor="line2">Street Address Line 2</Label>
-                        <Input placeholder="Optional" name='line2' value={form.line2} onChange={(e)=>changeHandler(e)}></Input>
+                        <Input type="text" placeholder="Optional" name='line2' value={form.line2} onChange={(e)=>changeHandler(e)}></Input>
                     </span>
                     <span>
-                    <Label htmlFor="state">State</Label>
-                        <Input placeholder="State" name='state' value={form.state} onChange={(e)=>changeHandler(e)} required></Input>
+                    <Label htmlFor="city">Zip Code</Label>
+                    <Input type="text" placeholder="Zip." name='zipCode' value={form.zipCode} onChange={(e)=>changeHandler(e)} required></Input>
                 </span>
                 </InputGroup>
                 <InputGroup>
                 <span>
                     <Label htmlFor="password">Password</Label>
-                        <Input placeholder="Password" name='password' value={form.password} onChange={(e)=>changeHandler(e)}></Input>
+                        <Input type="password" placeholder="Password" name='password' value={form.password} onChange={(e)=>changeHandler(e)}></Input>
                     </span>
                     <span>
                     <Label htmlFor="repeatPassword">Repeat New Password</Label>
-                        <Input placeholder="Repeat Password" name='repeatPassword' value={form.repeatPassword} onChange={(e)=>changeHandler(e)}></Input>
+                        <Input type="password" placeholder="Repeat Password" name='repeatPassword' value={form.repeatPassword} onChange={(e)=>changeHandler(e)}></Input>
                 </span>
                 </InputGroup>
                 <InputGroup>
