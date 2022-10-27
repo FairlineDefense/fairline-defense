44520eb247370c50dbf1a05b5a714c2a93cd9ec6
Jeffrey Wood <jwfirenze@gmail.com>
2022-10-27 16:52:42 -0400

    invoices rendering

--- client/components/Portal/MembershipSubPortal/Invoices.js
+++ client/components/Portal/MembershipSubPortal/Invoices.js
@@ -3,10 +3,39 @@ import styled from 'styled-components'
 import { useState, useEffect } from 'react'
 const Wrapper = styled.div`
 width: 100%;
-height: 100%;
+min-height: 100%;
+flex-direction: column;
+
+div:nth-child(odd) {
+    background-color: #E8F3F9;
+}
+`
+const Line = styled.div`
+width: 100%;
+display: flex;
+justify-content: space-between;
+padding: .2rem;
+margin: .5rem;
+`
+const Date = styled.span`
+width: fit-content;
+display: inline-block;
+`
+const Charge = styled.span`
+width: fit-content;
+display: inline-block;
+`
+const PDF = styled.span`
+width: fit-content;
+display: inline-block;
+
+a {
+    text-transform: uppercase;
+    color: var(--cyan);
+}
 `
 export default function Invoices() {
-    let [invoices, setInvoices] = useState([])
+    let [invoices, setInvoices] = useState([{}])
 
     const getInvoices = async () => {
         const res = await fetch('/payment/invoices', {
@@ -14,7 +43,7 @@ export default function Invoices() {
             headers: {'Content-type': 'application/json'}
         })
         const body = await res.json()
-        setInvoices(body)
+        setInvoices(body.data)
     }
 
     useEffect(()=> {
@@ -24,7 +53,12 @@ export default function Invoices() {
     console.log(invoices)
     return (
         <Wrapper>
-            Invoices
+            {invoices && invoices.map((line) =>
+            <Line key={line.id}>
+            <Date>{line.created}</Date>
+            <Charge>{line.amount_paid}</Charge>
+            <PDF><a href={line.hosted_invoice_url}>PDF</a></PDF>
+            </Line>)}
         </Wrapper>
     )
 }
