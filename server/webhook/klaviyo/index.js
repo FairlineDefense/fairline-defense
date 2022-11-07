da6fdb605e5da6b8bf80c63a0dde3b7fe7335084
Jeffrey Wood <jwfirenze@gmail.com>
2022-11-07 18:52:36 -0500

    models

---
+++ server/webhook/klaviyo/index.js
@@ -0,0 +1,20 @@
+const router = require('express').Router()
+const {User} = require('../../db/models')
+module.exports = router
+
+router.post('/verify-email', async (req, res, next) => {
+    try {
+      console.log('web hook called')
+      await User.update({emailVerified: true}, {where:{email: req.body.email}})
+    } catch (err) {
+      next(err)
+    }
+  })
+  
+  router.post('/verify-phone', async (req, res, next) => {
+      try {
+        await User.update({phoneVerified: true}, {where:{phone: req.body.phone}})
+      } catch (err) {
+        next(err)
+      }
+    })
