52db9dee32b92bcf215184522007c0f14e594a37
Jeffrey Wood <jwfirenze@gmail.com>
2022-11-08 11:30:18 -0500

    klaviyo api

--- server/webhook/klaviyo/index.js
+++ server/webhook/klaviyo/index.js
@@ -3,11 +3,10 @@ const router = require('express').Router()
 const {User} = require('../../db/models')
 module.exports = router
 
-router.post('/verify-email', express.raw({type: 'application/json'}), async (req, res, next) => {
+router.post('/verify-email', async (req, res, next) => {
   try {
-      const body = JSON.parse(req.body)
-      console.log(body)
-      await User.update({emailVerified: true}, {where:{email: body.email}})
+     console.log(req.body)
+      await User.update({emailVerified: true}, {where:{email: req.body.email}})
       return res.status(200).send()
     } catch (err) {
       next(err)
