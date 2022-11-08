8f85839f79b0e0be611d6d5c8d73df9da0b16bf6
Jeffrey Wood <jwfirenze@gmail.com>
2022-11-07 19:33:00 -0500

    removed express.raw

--- server/webhook/klaviyo/index.js
+++ server/webhook/klaviyo/index.js
@@ -3,7 +3,7 @@ const router = require('express').Router()
 const {User} = require('../../db/models')
 module.exports = router
 
-router.post('/verify-email', express.raw({type: 'application/json'}), async (req, res, next) => {
+router.post('/verify-email', async (req, res, next) => {
     try {
       console.log('web hook called', req.body)
       await User.update({emailVerified: true}, {where:{email: req.body.email}})
