4d52821ab22f2b5fa9a20d3e847967a5a1b74ea6
Jeffrey Wood <jwfirenze@gmail.com>
2022-11-07 19:39:40 -0500

    json parse

--- server/webhook/klaviyo/index.js
+++ server/webhook/klaviyo/index.js
@@ -3,9 +3,9 @@ const router = require('express').Router()
 const {User} = require('../../db/models')
 module.exports = router
 
-router.post('/verify-email', async (req, res, next) => {
-    try {
-      console.log('web hook called', req.body)
+router.post('/verify-email', express.raw({type: 'application/json'}), async (req, res, next) => {
+  try {
+    console.log('web hook called', JSON.parse(req.body))
       await User.update({emailVerified: true}, {where:{email: req.body.email}})
     } catch (err) {
       next(err)
