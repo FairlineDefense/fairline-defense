aee51b07328aacc99d21ab9cc97377e728177745
Jeffrey Wood <jwfirenze@gmail.com>
2022-11-16 12:23:31 -0500

    payment status url

--- client/components/Register/CheckoutForm.js
+++ client/components/Register/CheckoutForm.js
@@ -49,7 +49,7 @@ const CheckoutForm = () => {
       //`Elements` instance that was used to create the Payment Element
       elements,
       confirmParams: {
-        return_url: 'https://fairline-defense.herokuapp.com/webhook'
+        return_url: 'https://fairline-defense.herokuapp.com/paymentstatus'
       }
     })
 
