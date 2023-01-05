fe5b1b42d13d4dd6f77234bdbdbcdcf6ee42d64c
Jeffrey Wood <jwfirenze@gmail.com>
2023-01-05 14:23:39 -0500

    payment status url

--- client/components/Register/CheckoutForm.js
+++ client/components/Register/CheckoutForm.js
@@ -62,7 +62,7 @@ const CheckoutForm = () => {
       //`Elements` instance that was used to create the Payment Element
       elements,
       confirmParams: {
-        return_url: 'https://fairline-defense.herokuapp.com/paymentstatus'
+        return_url: process.env.NODE_ENV === 'production' ? 'https://fairlinedefense.jeffreylwood.com/paymentstatus' : 'http://localhost:8080/paymentstatus'
       }
     })
 
