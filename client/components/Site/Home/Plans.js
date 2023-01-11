a50bdcba78f8ffe532d65034834948e7136cda4d
Jeffrey Wood <jwfirenze@gmail.com>
2023-01-11 14:40:39 -0500

    plans page

--- client/components/Site/Home/Plans.js
+++ client/components/Site/Home/Plans.js
@@ -2,16 +2,17 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Section = styled.div`
-  height: fit-content;
+  height: 860px;
   max-width: 100vw;
   display: flex;
   flex-direction: column;
-  padding: 4rem;
+  padding: 6rem;
   align-items: center;
   background-color: #E8F3F9;
 
   @media (max-width: 800px) {
-    display: none;
+    height: 1150px;
+    padding: 2rem;
   }
 `
 const Wrapper = styled.div`
@@ -19,8 +20,12 @@ const Wrapper = styled.div`
   min-height: fit-content;
   flex-direction: row;
   justify-content: center;
-  align-items: center;
+  align-items: flex-start;
   width: 100%;
+
+  @media (max-width: 800px) {
+    flex-direction: column;
+  }
 `
 const Header = styled.div`
   font-size: 48px;
@@ -31,6 +36,8 @@ const Header = styled.div`
   margin-bottom: 1rem;
 
   @media (max-width: 800px) {
+    font-size: 32px;
+    line-height: 38px;
   }
 `
 const Subheader = styled.div`
@@ -40,6 +47,23 @@ const Subheader = styled.div`
   margin-bottom: 4rem;
 
   @media (max-width: 800px) {
+    font-size: 22px;
+  }
+`
+const Title = styled.div`
+font-size: 28px;
+line-height: 38px;
+text-align: center;
+letter-spacing: 0.2em;
+text-transform: uppercase;
+font-weight: 500;
+
+color: #132A4A;
+  color: var(--darkblue);
+  margin-bottom: 2rem;
+
+  @media (max-width: 800px) {
+    font-size: 24px;
   }
 `
 const Cyan = styled.span`
@@ -47,40 +71,88 @@ color: var(--cyan);
 font-size: inherit;
 font-weight: inherit;
 `
+const PlanWrapper = styled.div`
+display: flex;
+justify-content: center;
+align-items: flex-start;
+height: 450px;
+width: 400px;
+flex-direction: column;
+margin: 2rem;
+
+@media(max-width: 800px) {
+  margin: 0rem;
+  align-items: center;
+  width: 100%;
+  height: fit-content;
+  margin-bottom: 1rem;
+}
+`
 const Plan = styled.div`
-  height: 232px;
-  width: 284px;
+  min-height: 232px;
+  min-width: 400px;
   background: #223F67;
+  color: #fff;
+  display: block;
+  text-align: center;
   box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.25);
   border-radius: 20px;
-  color: #fff;
-  margin: 2rem;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
+  margin-bottom: 1rem;
 
   header {
     font-size: 60px;
     font-weight: 500;
+    margin: 1.5rem;
   }
   small {
     font-size: 30px;
+  }
+
+  @media(max-width: 800px) {
+    min-width: 280px;
   }
 `
 const Small = styled.div`
   font-size: 32px;
   line-height: 40px;
   color: var(--darkblue);
-  width: 620px;
-  padding-right: 300px;
+  width: 400px;
+  display: flex;
+  flex-direction: column;
+  height: 200px;
+
+  @media(max-width: 800px) {
+    width: 280px;
+    height: fit-content;
+    font-size: 24px;
+    line-height: 32px;
+  }
+`
+
+const Savings = styled.div`
+  font-size: 30px;
+  width: 400px;
+  display: block;
+  min-height: 200px;
+  color: var(--cyan);
+  text-align: center;
+  font-weight: bold;
+
+  @media(max-width: 800px) {
+    width: 280px;
+    height: fit-content;
+  }
 `
 const Arrow = styled.div`
-  width: 260px;
-  height: 200px;
-  background-image: url('./images/savings.png');
-  background-position: bottom;
-  background-repeat: no-repeat;
+  text-align: center;
+  width: 100%;
+  height: fit-content;
+  img {
+    position: relative;
+    top: -28px;
+    width: 120px;
+    height: 87px;
+  }
 `
 const Bold = styled.span`
   font-weight: 600;
@@ -91,21 +163,28 @@ export default function Plans() {
     <Section>
       <Header>No Complicated Plans & Options</Header>
       <Subheader>Peace of mind at <Cyan>$19.99</Cyan></Subheader>
-      <Header>Armed Citizen Plan</Header>
+      <Title>Armed Citizen Plan</Title>
       <Wrapper>
+        <PlanWrapper>
         <Plan>
           <header>$19.99</header>
           <small>Billed Monthly</small>
         </Plan>
+        <Small>
+        Add a spouse or family member for <Bold>$5 Monthly</Bold>
+      </Small>
+        </PlanWrapper>
+        <PlanWrapper>
         <Plan>
           <header>$199</header>
           <small>Billed Annually</small>
         </Plan>
-        {/* <Arrow /> */}
+        <Savings>
+            {/* <Arrow><img src="./images/cyanarrow.png" /></Arrow>*/}
+            Save $40!
+          </Savings>
+        </PlanWrapper>
       </Wrapper>
-      <Small>
-        Add a spouse or family member for <Bold>$5 Monthly</Bold>
-      </Small>
     </Section>
   )
 }
