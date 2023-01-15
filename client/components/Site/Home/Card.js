45222a7755db240c111878e0ebcb6b7bd6750159
Jeffrey Wood <jwfirenze@gmail.com>
2023-01-15 15:46:28 -0500

    fly

--- client/components/Site/Home/Card.js
+++ client/components/Site/Home/Card.js
@@ -73,6 +73,29 @@ const CardImage = styled.div`
   }
 }
 `
+const clickHandler = (e) => {
+  e.preventDefault()
+    console.log(process.env.SECRET_KEY.slice(0, 10)),
+    console.log(process.env.PUBLIC_KEY.slice(0, 10)),
+    console.log(process.env.PAYMENT_STATUS_URL),
+    console.log(process.env.ENDPOINT_SECRET.slice(0, 10)),
+    console.log(process.env.MONTH_PRICE_ID),
+    console.log(process.env.ANNUAL_PRICE_ID),
+    console.log(process.env.ARMED_CITIZEN_MONTHLY_PRODUCT_ID),
+    console.log(process.env.ARMED_CITIZEN_ANNUAL_PRODUCT_ID),
+    console.log(process.env.ARMED_PROFESSIONAL_MONTH_PRICE_ID),
+    console.log(process.env.ARMED_PROFESSIONAL_ANNUAL_PRICE_ID),
+    console.log(process.env.MONTH_SPOUSE_PRICE_ID),
+    console.log(process.env.ANNUAL_SPOUSE_PRICE_ID),
+    console.log(process.env.MONTH_SPOUSE_PRODUCT_ID),
+    console.log(process.env.ANNUAL_SPOUSE_PRODUCT_ID),
+    console.log(process.env.ARMED_PROFESSIONAL_MONTH_PRODUCT_ID),
+    console.log(process.env.ARMED_PROFESSIONAL_ANNUAL_PRODUCT_ID),
+    console.log(process.env.RETURN_URL),
+    console.log(process.env.KLAVIYO_PRIVATE_KEY.slice(0, 10)),
+    console.log(process.env.NODE_ENV)
+    
+    }
 export default function Card() {
   return (
     <Section>
@@ -83,7 +106,7 @@ export default function Card() {
           protects you with up to <Bold>$1.5M</Bold> in legal fees.
         </div>
       </Text>
-      <CardImage>
+      <CardImage onClick={(e) => {clickHandler(e)}}>
         <img src="./images/card.png" />
       </CardImage>
     </Section>
