812c4e7fed17feeaa0c81bd53d844f59799d3db7
Jeffrey Wood <jwfirenze@gmail.com>
2023-01-17 15:38:18 -0500

    Revert "collect sms consent"
    This reverts commit a11008609b8f7e23be4bc542d73be6168c081509.

--- server/klaviyo/index.js
+++ server/klaviyo/index.js
@@ -69,7 +69,7 @@ router.post('/create-account', async (req, res, next) => {
           data: {
             type: 'profile-subscription-bulk-create-job',
             attributes: {
-              subscriptions: [{email: req.body.email, phone_number: req.body.phone}],
+              subscriptions: [{email: req.body.email}],
               list_id: 'VXeuyy'
             }
           }
