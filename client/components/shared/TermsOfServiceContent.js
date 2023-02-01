cccbc5076abdf3496b1517dfb747ab2474e85960
Geena Lux <geenalux@Geenas-MBP.lan>
2023-02-01 17:48:40 -0600

    use shared component

---
+++ client/components/shared/TermsOfServiceContent.js
@@ -0,0 +1,343 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Section = styled.div`
+  margin-bottom: 1rem;
+`
+
+const List = styled.ul`
+  margin-left: 2rem;
+`
+
+const TermsOfServiceContent = () => {
+  return (
+    <div>
+      <Section>
+        <h3>1. Introduction</h3>
+        <p>
+          Fairline Defense is a company that is designed to protect you when you
+          are protecting yourself, your family, and your loved ones. We realize
+          how important privacy is to our membership. Protection is our business
+          and we have adopted the following policy to protect your information
+          and inform you of your options regarding how your personal information
+          is used online. This policy describes what types of information we
+          gather about you, how we use it, under what circumstances we disclose
+          it to third parties, and how you can update it. This page informs you
+          of our policies regarding the collection, use and disclosure of
+          personal data when you use our Service and the choices you have
+          associated with that data.
+        </p>
+        <p>
+          Fairline Defense is referred to herein as (“Defense,” “us”, “we”, or
+          “our”) and the members we provided subscription-based services
+          referred to herein as (“customer”, “you”, “your,” or “member(s)” and
+          the information within the Fairline Defense website, the call center,
+          mobile application, or contact with agents of Fairline Defense
+          (hereinafter referred to as the “Service(s)”).
+        </p>
+        <p>
+          This Privacy Policy is additional to and does not replace or supersede
+          our Terms of Service, which can be found at{' '}
+          <a href="https://www.fairlinedefense.com/terms-of-service/">
+            https://www.fairlinedefense.com/terms-of-service/
+          </a>
+          . Please read this Privacy Policy carefully. If any term in this
+          Privacy Policy is unacceptable to you, please do not use the Website
+          or provide us with any Personal Information.
+        </p>
+        <p>
+          We use your data to provide and improve the Service. By using the
+          Service, you agree to the collection and use of information in
+          accordance with this policy. Unless otherwise defined in this Privacy
+          Policy, the terms used in this Privacy Policy have the same meanings
+          as in our Terms and Conditions.
+        </p>
+        <p>
+          This is not just a commitment to protect your privacy, this is a
+          pledge to actively protect the information you provide to us as part
+          of your membership. We are strongly committed to safeguarding consumer
+          privacy on our website. The only information our website will obtain
+          from you is information we need to process credit card charges of
+          membership fees or for other purchases. That information is never
+          revealed, resold, or made public without your consent. Any requests
+          from a business, person, or 3rd party vendor for your information is
+          likely to get lost in our legal department and any search warrants,
+          court orders, or subpoenas will be actively challenged and fought.
+        </p>
+        <p>
+          We collect information from you when you register on our site and
+          gathers data when you participate with content there. We use this
+          information to help our members more efficiently and ensure that
+          members have full freedom to access the content made available to
+          them. This online privacy policy applies only to information collected
+          through our site and not to information collected offline.
+        </p>
+      </Section>
+      <Section>
+        <h3>2. Definitions</h3>
+        <List>
+          <li>
+            CookiesSmall files stored on your device (computer or mobile
+            device).
+          </li>
+          <li>
+            ServiceService means the http://www.fairlinedefense.com website and
+            the mobile application operated by Fairline Defense.
+          </li>
+          <li>
+            Personal DataGenerally information that identifies, relates to,
+            describes, is reasonably capable of being associated with, or could
+            reasonably be linked, directly or indirectly, with a particular
+            person (for example, name, address, telephone number, Social
+            Security number, and driver's license number.)
+          </li>
+          <li>
+            Usage DataData collected automatically either generated using the
+            Service or from the Service infrastructure itself (for example, the
+            duration of a page visit).
+          </li>
+          <li>States and Specific Rights</li>
+        </List>
+        <p>
+          In certain circumstances, our residence may change your rights
+          regarding your protections.
+        </p>
+      </Section>
+      <Section>
+        <h3>3. Cookies</h3>
+        <p>
+          When you log into our site, we store a temporary “cookie” in the
+          browser files of your computer. Cookies are small pieces of text sent
+          to your browser by a website you visit. They help that website
+          remember information about your visit, which can both make it easier
+          to visit the site again and make the site more useful to you. We use
+          cookies and similar tracking technologies to track the activity on our
+          Service and we hold certain information. The data geeks explained this
+          to us, so we must make sure we passed along this information to you.
+          Examples of Cookies we use:
+        </p>
+        <List>
+          <li>
+            Session Cookies. We use Session Cookies to operate our Service.
+          </li>
+          <li>
+            Preference Cookies. We use Preference Cookies to remember your
+            preferences and various settings.
+          </li>
+          <li>
+            Security Cookies. We use Security Cookies for security purposes.
+          </li>
+          <li>
+            Advertising Cookies. Advertising Cookies are used to serve you with
+            advertisements that may be relevant to you and your interests.
+          </li>
+        </List>
+        <p>
+          You can instruct your browser to refuse all cookies or to indicate
+          when a cookie is being sent. However, if you do not accept cookies,
+          you may not be able to use some portions of our Service.
+        </p>
+        <p>
+          Third Parties (i.e. Google, Microsoft Explore, Safari): Third party
+          vendors, including use cookies to serve ads based on your prior visits
+          to this website. You have the option to opt out of these cookies by
+          visiting your browsers setting or though other programs that ensure
+          cookies are not saved or stored. Links to other websites: we work to
+          ensure that the content on our website and the links to other websites
+          are safe and properly vetted for use. However, as a reminder we do not
+          control the content of these third parties, and we cannot control if
+          they are following their own privacy policies. The same goes for any
+          service you choose from a third party or service from a third-party
+          you interact with before, after, or during your interactions with
+          Defense.
+        </p>
+        <p>
+          To provide visitors with greater value, some of our pages may provide
+          links to various third-party websites. In no case is member
+          information or 'cookies' provided to the third-party sites. However,
+          keep in mind that many sites do attempt to capture visitor information
+          when you browse those sites. Once you leave our site, we are not able
+          to alert you should another site attempt to gather information from
+          your browser.
+        </p>
+      </Section>
+      <Section>
+        <h3>4. Service</h3>
+        <p>
+          More details are provided in our terms of service policy. Bottomline
+          is Defense has put reasonable security measures in place to protect
+          the information under our control to prevent the misuse, loss, or
+          change of the information in our custody. We work to protect the
+          information we receive from loss, theft, unauthorized use, and
+          modification and cannot guarantee its absolute security. While we
+          employ strict security measures to safeguard online transactions and
+          personal information is stored in a secured database we cannot
+          guarantee absolute security, because no system is error free or fully
+          secure.
+        </p>
+      </Section>
+      <Section>
+        <h3>5. Personal Data</h3>
+        <p>
+          Our site gives users the opportunity to opt-in to receive
+          communications from us. You can be assured that we never reveal, sell,
+          or make available your email address to anyone for any purpose.
+        </p>
+        <p>
+          This site also gives users the option to remove their information from
+          our database to stop receiving communications or our service. No
+          mobile information will be shared with third parties for
+          marketing/promotional purposes. All the above categories exclude text
+          messaging originator opt-in data and consent; this information will
+          not be shared with any third parties. The opt-in is not transferable
+          to another party involved in the process. While using our Service, we
+          may ask you to provide us with certain personally identifiable
+          information that can be used to contact or identify you (“Personal
+          Data”). Personally identifiable information may include, but is not
+          limited to:
+        </p>
+
+        <List>
+          <li>Email address</li>
+          <li>First name and last name </li>
+          <li>Phone number</li>
+          <li>Address, State, Province, ZIP/Postal code, City</li>
+          <li>Cookies and Usage Data</li>
+        </List>
+        <p>
+          We may use your Personal Data to contact you with newsletters,
+          marketing or promotional materials and other information that may be
+          of interest to you. You may opt out of receiving any, or all, of these
+          communications from us by following the unsubscribe link or the
+          instructions provided in any email we send. We may also collect
+          information that your browser sends whenever you visit our Service or
+          when you access the Service by or through a mobile device (“Usage
+          Data”).
+        </p>
+        <p>
+          This Usage Data may include information such as your computer's
+          Internet Protocol address (IP address), browser type, browser version,
+          the pages of our Service that you visit, the time and date of your
+          visit, the time spent on those pages, unique device identifiers and
+          other diagnostic data. When you access the Service with a mobile
+          device, this Usage Data may include information such as the type of
+          mobile device you use, your mobile device unique ID, the IP address of
+          your mobile device, your mobile operating system, the type of mobile
+          Internet browser you use, unique device identifiers and other
+          diagnostic data.
+        </p>
+        <p>
+          We should not even have a section regarding to location data. You
+          don't want us to have it and we don't want it. The only reason we
+          might have access to your location data is because a third-party
+          vendor like Google saved it and tried to give it to us.
+        </p>
+      </Section>
+      <Section>
+        <h3>6. Usage Data</h3>
+        <p>
+          We use the information provided by you and your computer to provide
+          and maintain our services. We use analytical services to determine the
+          effectiveness of our content, what interests our users, and the
+          functionality of our Services. We use this information to notify you
+          of changes to our services, provide customer support, and improve
+          services. To improve functionality, services, and business processes
+          we use web beacons, tracking pixels, and similar technologies to
+          understand more about Website visitors and count visitor numbers.
+        </p>
+        <p>
+          We will use your data to draft, develop, or perform a contract with
+          you. Your data will be used to execute our overall business goals
+          which is not superseded by law, or as needed to comply with the law.
+          We will retain and use your Personal Data to the extent necessary to
+          comply with our legal obligations (for example, if we are required to
+          retain your data to comply with applicable laws), resolve disputes and
+          enforce our legal agreements and policies. While this data is
+          generally retained for shorter period, the retention duration of this
+          dada will be extended if it can be used to improve functionality, or
+          the security of the services provided.
+        </p>
+        <p>
+          We may employ third party companies and individuals to facilitate our
+          Service (“Service Providers”), provide the Service on our behalf,
+          perform Service-related functions, or assist us in analyzing how our
+          Service is used. These third parties have access to your Personal Data
+          only to perform these tasks on our behalf and are obligated not to
+          disclose or use it for any other purpose. For the purposes of
+          improving business processes and company security. We do not support
+          Do Not Track (“DNT”). Do Not Track is a preference you can set in your
+          web browser to inform websites that you do not want to be tracked. You
+          can enable or disable Do Not Track by visiting the Preferences or
+          Settings page of your web browser.
+        </p>
+        <p>
+          Our Service does not address anyone under the age of 18 (“Children”).
+          If you are under the age of 18, please do not use our Service or
+          access our website or send any user information to us. We do not
+          knowingly collect personally identifiable information from anyone
+          under the age of 18. If you are a parent or guardian and you are aware
+          that your Child has provided us with Personal Data, please contact us.
+          If we become aware that we have collected Personal Data from children
+          without verification of parental consent, we take steps to remove that
+          information from our servers.
+        </p>
+      </Section>
+      <Section>
+        <h3>7. States and Specific Rights</h3>
+        <p>
+          Subject to certain limitation and based on where you reside. In
+          certain circumstances, you may have the following data protection
+          rights:
+        </p>
+        <List>
+          <li>
+            The right to access or know. Whenever made possible, you can access,
+            update or request deletion of your Personal Data directly within
+            your account settings section. You have the right to be provided
+            with a copy of the information we have on you in a structured,
+            readable, and commonly used format. If you are unable to review the
+            information displayed when you login to your account, please contact
+            us to assist you.
+          </li>
+          <li>
+            The right of verify. You have the right to have your information
+            verified if that information is inaccurate or incomplete.
+          </li>
+          <li>
+            The right to object. We do not use sensitive personal information
+            for any purpose that is not necessary to provide our Services You
+            have the right to request the deletion, limit or restrict the use of
+            your Personal Information, subject to certain exceptions.
+          </li>
+          <li>
+            The right to withdraw consent. You also have the right to withdraw
+            your consent at any time after we relied on your consent to process
+            your personal information.
+          </li>
+        </List>
+        <p>
+          Please note that we may ask you to verify your identity before
+          responding to such requests.
+        </p>
+      </Section>
+      <Section>
+        <h3>8. Changes to This Privacy Policy</h3>
+        <p>
+          We may update our Privacy Policy from time to time. You are
+          responsible for periodically monitoring and reviewing any updates to
+          this Privacy Policy. The date of this Privacy Policy was last revised
+          is identified at the top of the page. Unless a change in any
+          applicable law requires immediate modification, all amended terms
+          automatically take effect after they are initially posted on our
+          website.
+        </p>
+        <p>
+          Contact Us If you have any questions about this Privacy Policy, please
+          contact us through customer service.
+        </p>
+      </Section>
+    </div>
+  )
+}
+
+export default TermsOfServiceContent
